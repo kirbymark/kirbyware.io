5473de2270472061c7bcf3198132125faca09db9
Mark Kirby <mark@kirbyware.com>
2023-06-06 10:26:57 -0400

    updated before flexbox deepdive

--- projects/AwesomeProject/App.js
+++ projects/AwesomeProject/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my Awesome Project</Text>
-      <Text>Hello world changes more now</Text>
+      <Text style={styles.textstyle1}>This is my Awesome Project</Text>
+      <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Enter a goal" />
+          <Button title="Add Goal" color="#599284"/>
+      </View>
+      <View>
+          <Text>List of Goals</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +23,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#1f3',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
   },
+  textstyle1: {
+    margin: 16, 
+    borderWidth: 1, 
+    borderColor: 'orange', 
+    padding: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+  }
 });
