d84d7c753a49f967530f2559973873c5ef8694ac
Mark Kirby <mark@kirbyware.com>
2023-06-06 10:46:20 -0400

    playing with flex

--- projects/AwesomeProject/App.js
+++ projects/AwesomeProject/App.js
@@ -12,7 +12,21 @@ export default function App() {
       <View>
           <Text>List of Goals</Text>
       </View>
+      
+      <View style={styles.FlexBox}>
+        <View style={styles.Box1}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.Box2}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.Box3}>
+          <Text>3</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -23,7 +37,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#1f3',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 50,
+    padding: 20,
   },
   textstyle1: {
     margin: 16, 
@@ -34,6 +48,32 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  Box1: {
+    backgroundColor: 'red',
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Box2: {
+    backgroundColor: 'green',
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Box3: {
+    backgroundColor: 'cyan',
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  FlexBox: {
+    padding: 5,
+    flexDirection: 'row',
+    width: '80%',
+    height: 300,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
   },
   textInput: {
     borderWidth: 1,
