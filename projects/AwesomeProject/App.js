b7007e244e3a86097f28ce79846a533c226153a0
Mark Kirby <mark@kirbyware.com>
2023-06-06 11:02:33 -0400

    layout complete

--- projects/AwesomeProject/App.js
+++ projects/AwesomeProject/App.js
@@ -3,41 +3,24 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textstyle1}>This is my Awesome Project</Text>
+    <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-          <TextInput style={styles.textInput} placeholder="Enter a goal" />
+          <TextInput style={styles.textInput} placeholder="Enter a goal!" />
           <Button title="Add Goal" color="#599284"/>
       </View>
-      <View>
-          <Text>List of Goals</Text>
+      <View style={styles.goalsContainer}>
+          <Text>List of Goals...</Text>
       </View>
-      
-      <View style={styles.FlexBox}>
-        <View style={styles.Box1}>
-          <Text>1</Text>
-        </View>
-        <View style={styles.Box2}>
-          <Text>2</Text>
-        </View>
-        <View style={styles.Box3}>
-          <Text>3</Text>
-        </View>
-      </View>
-
-      <StatusBar style="auto" />
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#1f3',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    paddingTop: 20,
+    paddingHorizontal: 16,
   },
   textstyle1: {
     margin: 16, 
@@ -47,38 +30,21 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: 'row',
+    flex : 1,
     justifyContent: 'space-between',
-  },
-  Box1: {
-    backgroundColor: 'red',
-    flex: 1, 
-    justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
-  Box2: {
-    backgroundColor: 'green',
-    flex: 1, 
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  Box3: {
-    backgroundColor: 'cyan',
-    flex: 1, 
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  FlexBox: {
-    padding: 5,
-    flexDirection: 'row',
-    width: '80%',
-    height: 300,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
+  goalsContainer: {
+    flex: 5,
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
-  }
+    padding: 8,
+  },
 });
