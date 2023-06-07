6765b4ce9927fae776e008da5df1d13b4243e21f
Mark Kirby <mark@kirbyware.com>
2023-06-07 13:21:41 -0400

    add material ui and passing childern components

--- projects/learn-react/my-reactdemo/src/App.tsx
+++ projects/learn-react/my-reactdemo/src/App.tsx
@@ -1,4 +1,6 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import { Divider, Chip, Box } from "@mui/material";
 
 function App() {
   let items = [
@@ -11,11 +13,28 @@ function App() {
 
   const handleSelectItem = (item: string) => {
     console.log(item);
-  }
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Box
+        sx={{
+          width: "100%",
+          backgroundColor: "grey",
+          height: 100,
+        }}
+      />
+      <Divider>
+        <Chip label="CHIP" />
+      </Divider>
+      <Alert>
+        <span>This is an alert</span>
+      </Alert>
     </div>
   );
 }
